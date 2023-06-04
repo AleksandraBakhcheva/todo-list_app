98588e16053a6f6e668acac810a19ea70b60ec62
Aleksandra <alexandra.bakhcheva@gmail.com>
2023-06-04 17:23:31 +0500

    add function for input validation

---
+++ src/assets/utils/Functions.ts
@@ -0,0 +1,27 @@
+import { Validatable } from "./Interfaces";
+
+export function validate(validatableInput: Validatable): boolean {
+  let isValid = true;
+
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+  }
+
+  if (
+    validatableInput.minLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length >= validatableInput.minLength;
+  }
+
+  if (
+    validatableInput.maxLength != null &&
+    typeof validatableInput.value === "string"
+  ) {
+    isValid =
+      isValid && validatableInput.value.length <= validatableInput.maxLength;
+  }
+
+  return isValid;
+}
