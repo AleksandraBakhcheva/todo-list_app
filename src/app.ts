acebc4814a9240f8302bdbb61ac7c63db73df133
Aleksandra <alexandra.bakhcheva@gmail.com>
2023-06-06 11:43:19 +0500

    add app + built js

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,6 @@
+import { TodoInput } from "./assets/classes/TodoInput";
+import { TodoList } from "./assets/classes/TodoList";
 
+new TodoInput();
+new TodoList("active");
+new TodoList("finished");
